c0a341616e05633d43c2140e9e625382f5fc665b
Tyhra Barreto <tyhra4@gmail.com>
2022-11-15 21:11:18 -0500

    Started Login/Register Routes

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
 const URI = process.env.ATLAS_URI;
 mongoose.connect(URI,
     err => {
@@ -20,6 +23,26 @@ const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully.")
 });
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', { name: 'Kyle'})
+})
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs')
+})
+
+app.post('/login', (req, res) => {
+  
+})
+
+app.get('/register', (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+  req.body.name
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
