ffca62fd3315981ef3d4cd5e03d5cefde6efc502
Tyhra Barreto <tyhra4@gmail.com>
2022-11-21 20:53:55 -0500

    Created routes

---
+++ frontend/src/NavBar.js
@@ -0,0 +1,27 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
+export default function NavBar() {
+    return (
+        <nav className="nav">
+            <Link to="/" className="Home">
+                Arcade
+            </Link>
+            <ul>
+                <CustomLink to="/about">About</CustomLink>
+                <CustomLink to="/projects">Projects</CustomLink>
+            </ul>
+        </nav>
+    )
+}
+
+function CustomLink({to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
+    )
+}
